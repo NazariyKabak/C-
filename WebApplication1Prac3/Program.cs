7c016beb93244cb0a7005acf73509879dbf2ac3e
nazarii <nazar27062002@gmail.com>
2024-12-19 19:29:48 +0000

    Fix

--- WebApplication1Prac3/Program.cs
+++ WebApplication1Prac3/Program.cs
@@ -3,20 +3,20 @@ using WebApplication1Prac3.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Підключення до бази даних
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Додаємо контролери
+
 builder.Services.AddControllers();
 
-// Додаємо Swagger
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Налаштування Swagger
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -26,7 +26,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 
-// Підключення контролерів
+
 app.MapControllers();
 
 app.Run();
